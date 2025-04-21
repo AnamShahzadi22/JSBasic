612c3b5d1820aa597042b7dbabcfb7565d334189
AnamShahzadi22 <120821901+AnamShahzadi22@users.noreply.github.com>
2025-04-21 17:54:10 +0000

    Conversion of Data Types

---
+++ JSBasics/datatypeconversion.js
@@ -0,0 +1,47 @@
+//conversion and Operations
+///Number Conversion 
+//let score =33;
+///String
+//let score = "33"
+//if not a number format then mean not
+//  convert to number like 123abc it is nan
+//let score = "33abc"
+//let score = null;
+let score = undefined ;
+
+///type of it 
+console.log(typeof(score));
+//Or same with fn and without fun:
+console.log(typeof score);
+///conversion to number from string score  
+// capital format for conversion
+ let valueInNumber = Number(score);
+ ///undefine nan but its also number 
+ console.log(typeof valueInNumber); ///number 
+ ///it is also a special type nan not relay on number is js so checkit must
+ console.log(valueInNumber); //Nan
+
+ ///nan type is number but its not a number
+
+
+ ///Boolean Conversion 
+ //0 empty string null undefined also false 
+ //let isLoggedIn = 1;
+ let isLoggedIn = undefined;
+ ///convert to bool
+ let booleanLoggedIn = Boolean(isLoggedIn)
+ console.log(typeof booleanLoggedIn);
+ console.log(booleanLoggedIn);
+
+ ///String Conversion
+ let someNumber =33;
+ let stringNumber = String(someNumber);
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+ 
+ 
+ 
+ 
+
+
+ 
