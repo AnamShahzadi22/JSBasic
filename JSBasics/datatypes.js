f6aa72efcbea5715bb2f7d25939c07224446862c
AnamShahzadi22 <120821901+AnamShahzadi22@users.noreply.github.com>
2025-04-21 16:43:04 +0000

    data types

---
+++ JSBasics/datatypes.js
@@ -0,0 +1,31 @@
+/// for that standard follow for every old and new code
+///give standard to alll new and old code
+//  because they run on sam engine for that use
+"use strict";  //treat as new version of js  treat as new standard
+///if we use it we dont use  nostrict this is not exist in js
+
+///we are using node js not browser in browser pop up shown
+//alert("Hello");
+
+///data types Priimitve Data TYpes
+///Number +> 2 to power 53
+// , BigInt too lengthy number trade etc
+// ,String = "",
+// Boolean true,false
+// ,Null stand alone value is null empty value represnetation 
+///let state = null; empty 
+//  ,Undefined  not define initlize value,
+//Symbol used in React for unique COmponents uniqueness 
+///Non Primitive
+//Objects
+
+let name = "anam"
+let age =  22;
+let isLoggedIn = false;
+console.log(typeof("abc"))//string
+///also variable type 
+console.log(typeof(isLoggedIn));//bool
+console.log(typeof(null));//type object h
+console.log(typeof(undefined)); //undefine
+
+
--- JSBasics/test.js
+++ JSBasics/test.js
@@ -1,1 +1,20 @@
 console.log("Anam");
+///variables and their types 
+///not changeable
+const accountId = 144557;
+///changeable
+///Prefer not to use var isue in block scope and functional scope
+let accountEmail = "anam@gmail.com";
+var accountPassword ="12345";
+//not recomended
+accountCity = "abc";
+accountEmail = "abc";
+accountPassword = "4444";
+///i dont know value of  this  its predefinded undefined not initlized mean undefine
+let accountState;
+//not recomended
+accountCity = "bbc"
+//accountId = 2  not allowed
+console.log(accountId);
+//another method to print all variables table structure of printing
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
