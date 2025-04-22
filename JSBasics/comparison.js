4cefcceac5307c9b16f1795ddd655ea7b32bb2d0
AnamShahzadi22 <120821901+AnamShahzadi22@users.noreply.github.com>
2025-04-22 05:56:51 +0000

    comparison js

---
+++ JSBasics/comparison.js
@@ -0,0 +1,37 @@
+//comparison less then greater equal etc 
+//answer in bool true or false
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+//Porblem :  not compare same data type
+//automaticaly js convert string to number
+//not recommended
+//String Comparison
+console.log("2">1);
+console.log("02">1);
+//null comparison
+//null convert to only comperison situation 0
+console.log(null >=0);
+///equality check js not convert null to number
+
+console.log(null==0);
+console.log(null>0);
+//undefined is only equal to null in all other case its false
+console.log(undefined==0);
+console.log(undefined>=0);
+console.log(undefined >0);
+console.log(undefined== null);
+//in double equal auto type  conversion  but in === not 
+//strict check check value and data type both 
+console.log("2"===2); //false
+
+
+
+
+
+
+
+
+
