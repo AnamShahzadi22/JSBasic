bffe5eda09a45551e0e23f43efbfc214639605bf
AnamShahzadi22 <120821901+AnamShahzadi22@users.noreply.github.com>
2025-04-22 05:34:11 +0000

    Operations in JavaScipt

--- JSBasics/datatypeconversion.js
+++ JSBasics/datatypeconversion.js
@@ -1,4 +1,5 @@
 //conversion and Operations
+// ***************Conversion***********
 ///Number Conversion 
 //let score =33;
 ///String
@@ -7,33 +8,33 @@
 //  convert to number like 123abc it is nan
 //let score = "33abc"
 //let score = null;
-let score = undefined ;
+// let score = undefined ;
 
-///type of it 
+// ///type of it 
 console.log(typeof(score));
-//Or same with fn and without fun:
+// //Or same with fn and without fun:
 console.log(typeof score);
-///conversion to number from string score  
-// capital format for conversion
+// ///conversion to number from string score  
+// // capital format for conversion
  let valueInNumber = Number(score);
- ///undefine nan but its also number 
+//  ///undefine nan but its also number 
  console.log(typeof valueInNumber); ///number 
- ///it is also a special type nan not relay on number is js so checkit must
+//  ///it is also a special type nan not relay on number is js so checkit must
  console.log(valueInNumber); //Nan
 
- ///nan type is number but its not a number
+//  ///nan type is number but its not a number
 
 
- ///Boolean Conversion 
- //0 empty string null undefined also false 
+//  ///Boolean Conversion 
+//  //0 empty string null undefined also false 
  //let isLoggedIn = 1;
  let isLoggedIn = undefined;
- ///convert to bool
+//  ///convert to bool
  let booleanLoggedIn = Boolean(isLoggedIn)
  console.log(typeof booleanLoggedIn);
  console.log(booleanLoggedIn);
 
- ///String Conversion
+//  ///String Conversion
  let someNumber =33;
  let stringNumber = String(someNumber);
  console.log(stringNumber);
@@ -41,6 +42,56 @@ console.log(typeof score);
  
  
  
+ //************Operations **********/
+ //Arithematic Operations Logical etc
+ //negative value 
+ let value = 3;
+ let negValue = -value
+ console.log(negValue);
+//  //Arithematic Operations
+ console.log(2+2);
+ console.log(2-2);
+  console.log(2*2);
+   console.log(2/2);
+   console.log(2**3);
+   console.log(3%2);
+//  ///with add we only concatinate string only add not any other operations
+let str1 = "hello";
+let str2 = " Anam"
+let str3 = str1+ str2
+console.log(str3);
+// ///major problem concatinating with string
+// //rule if number add with string it become string in js
+console.log("1"+2);
+console.log(1+ "2");
+// //12 become string they add with last 2 it is also string 
+console.log("1"+2+2);
+// ///here first number add its 3 then add like string
+console.log(1+2+"2");
+
+//tricky  special type conversion 
+//with + convesion to number of the statment 
+console.log(true);
+///output 1
+console.log(+true);
+console.log(+"");
+//Assignment  Operation
+let num1,num2,num3
+//not recommended 
+num1 = num2 = num3 = 2+2
+///changes in prefix and post fix  
+//post fix value first assingn then increment
+//prefix value first increment then assign to other variable
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
  
 
 
